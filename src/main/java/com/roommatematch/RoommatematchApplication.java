27a387acc4e0925343e04518a0d89d6c6ac1fd6f
Soloplay31@ <polatozdemirs@gmail.com>
2026-03-29 08:05:29 +0300

    Initial Project Setup

---
+++ src/main/java/com/roommatematch/RoommatematchApplication.java
@@ -0,0 +1,13 @@
+package com.roommatematch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoommatematchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoommatematchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/roommatematch/RoommatematchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roommatematch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoommatematchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
